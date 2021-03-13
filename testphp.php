26ed6c279bd64028243ff7eca85e43188ffa47fc
Mint Lin <lin_xinyue@outlook.com>
2021-03-13 15:36:46 -0500

    pushcode

---
+++ testphp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>Mint's home page here! </h1>
+        <?php
+            echo "Hi, I'm a PHP script!<BR><BR>";
+            $i = 0;
+            echo "The variable \"i\" is " . $i;
+
+            $x = 2;
+            $y = 3;
+            $z = $x+$y;
+            echo "<BR><BR>";
+            echo $x . " plus " . $y . " is: " . $z;
+        ?>
+
+    </body>
+
+</html>
